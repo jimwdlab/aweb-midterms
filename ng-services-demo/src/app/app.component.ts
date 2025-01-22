e39a72bb079f21d8cd9c1a21d4bbd880d3c51389
Jimwel Valdez <jimvdz@protonmail.com>
2025-01-22 11:14:59 +0800

    Finish Lab Act 4

--- ng-services-demo/src/app/app.component.ts
+++ ng-services-demo/src/app/app.component.ts
@@ -1,44 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from './employee.service';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-services-demo';
-  public employees = [{
-    id: 101,
-    firstname: 'Joseph',
-    lastname: 'Dizon',
-    email: 'jdizon@hau.edu.ph',
-  },
-  {
-    id: 102,
-    firstname: 'James',
-    lastname: 'Atienza',
-    email: 'jatienza@hau.edu.ph',
-  },
-  {
-    id: 103,
-    firstname: 'John',
-    lastname: 'Cena',
-    email: 'jcena@hau.edu.ph',
-  },
-  {
-    id: 104,
-    firstname: 'Robert',
-    lastname: 'Quintana',
-    email: 'rquintana@hau.edu.ph',
-  },
-  {
-    id: 105,
-    firstname: 'Jimwel',
-    lastname: 'Valdez',
-    email: 'jlvaldez@student.hau.edu.ph',
+  public employees: {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+  }[] = [];
+
+  public products: {
+    productid: string;
+    name: string;
+    description: string;
+    price: string;
+  }[] = [];
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _productService: ProductsService
+  ) {}
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.products = this._productService.getProducts();
   }
-  ];
+  
 }
