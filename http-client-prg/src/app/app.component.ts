04a47e366bafb0080ac32320ccda05b5e788aa26
Jimwel Valdez <175762163+jimwdlab@users.noreply.github.com>
2025-02-08 20:32:58 +0800

    Finalize W11 challenge activity

--- http-client-prg/src/app/app.component.ts
+++ http-client-prg/src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpclientService } from './httpclient.service';
+import { FormsModule } from '@angular/forms';
+import { NgxSearchFilterModule } from 'ngx-search-filter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, NgxSearchFilterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'http-client-prg';
+  httptodos: any;
+  searchString: any;
+
+  constructor(private httpclientList: HttpclientService) {
+    this.httpclientList.getTodosRemotely().subscribe((data:any) => {
+      this.httptodos = data;
+    })
+  }
 }
--- http-client-prg/src/app/httpclient.service.ts
+++ http-client-prg/src/app/httpclient.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpclientService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getTodosRemotely(): Observable<any> {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos');
+  }
 }
